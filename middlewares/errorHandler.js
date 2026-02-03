f29d0077cef138bcae8f21ce2f7d7a42ae234fa6
Capybarajack <asdfg703703703@ceci.com.tw>
2026-02-03 20:17:02 +0800

    fix: prevent invalid status codes; add 413/502

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,11 +2,17 @@ const ApiError = require('../utils/ApiError');
 const httpStatus = require('../utils/httpStatus');
 
 const errorHandler = (err, req, res, next) => {
-  let { statusCode, message } = err;
+  let { statusCode, message } = err || {};
 
+  // If it's not our ApiError, normalize.
   if (!(err instanceof ApiError)) {
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     message = 'An unexpected error occurred';
+  }
+
+  // Defensive: never allow undefined/invalid status codes.
+  if (!Number.isInteger(statusCode) || statusCode < 100 || statusCode > 599) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
   }
 
   res.status(statusCode).json({
